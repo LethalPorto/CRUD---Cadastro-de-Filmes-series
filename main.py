e469da74208d19c731001cd5a1fab592b714883a
MosiasFrtizCarmo <lethalporto@gmail.com>
2025-11-13 16:06:33 -0300

    Improve input handling in main.py
    Added input validation for menu options.

--- main.py
+++ main.py
@@ -7,20 +7,27 @@ while True:
     print("="*30)
     print("            Menu")
     print("=" * 30, '\n')
-    print("[1] Cadastrar filme/serie \n[2] Atualizar filme \n[3] Leitura dos filmes  \n[4] Deletar um filme \n[0] Sair")
+    print()
 
-    opcao = input("\nOpção: ")
+    while True:
+        try:
+            print("[1] Cadastrar filme/serie \n[2] Atualizar filme \n[3] Leitura dos filmes  \n[4] Deletar um filme \n[0] Sair\n")
+            opcao = int(input("Opção: "))
+            if opcao < 0 or opcao > 4:
+                print('Erro: Digite apenas números entre 0 e 4...\n')
+            else:
+                break
+        except ValueError:
+            print('Erro: Digite apenas números...\n')
 
-    if opcao == "1":
+    if opcao == 1:
         app_filmes.adicionar(filme_series)
-    elif opcao == "2":
+    elif opcao == 2:
         app_filmes.atualizar(filme_series)
-    elif opcao == "3":
+    elif opcao == 3:
         app_filmes.ler(filme_series)
-    elif opcao == "4":
+    elif opcao == 4:
         app_filmes.deletar(filme_series)
-    elif opcao == "0":
+    elif opcao == 0:
         print("Adeus!")
         break
-    else:
-        print("Erro: Opção inválida...\n")
