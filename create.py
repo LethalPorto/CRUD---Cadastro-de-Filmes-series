cc49716f84346167fbb3e041cb69c3c104d90e9f
MosiasFrtizCarmo <lethalporto@gmail.com>
2025-11-17 23:17:12 -0300

    Update create.py

--- create.py
+++ create.py
@@ -1,13 +1,21 @@
 # Henrique - create.py
+# função de tratamento (otimização)
+def tratamento_vazio(entrada):
+    while True:
+        valor = input(entrada).strip()
+        if valor != "":
+            return valor
+        print("Erro: Obrigatório inserir dados...")
+
 # Função para criar novo filme
 def adicionar(filmes_series):
-    filme_serie = []
+    print('-' * 30)
+    print('          Adicionar')
+    print('-' * 30)
     # Pegando os dados
     while True:
-        titulo = input("Título: ")
-        if titulo == '':
-            print("Erro: Digite um título...")
-            continue
+        titulo = tratamento_vazio("Título: ")
+
         tem = False
         for item in filmes_series:
             if item[0].lower().strip() == titulo.lower().strip():
@@ -17,35 +25,27 @@ def adicionar(filmes_series):
             print("Erro: Esse filme/serie já existe...")
         else:
             break
-    while True:
-        genero = input("Gênero: ")
-        if genero == '':
-            print("Erro: Digite um gênero...")
-            continue
-        else:
-            break
+
+    genero = tratamento_vazio("Gênero: ")
 
     while True:
+        ano_str = tratamento_vazio("Ano de Lançamento: ")
         try:
-            ano = int(input("Ano de Lançamento: "))
+            ano = int(ano_str)
             break
         except ValueError:
             print("Erro: Digite um número válido...")
 
-    plataforma = input("Plataforma: ").strip()
+    plataforma = tratamento_vazio("Plataforma: ")
 
     # colocando os valores na linha
-    filme_serie.append(titulo)
-    filme_serie.append(genero)
-    filme_serie.append(ano)
-    filme_serie.append(plataforma)
-
+    filme_serie = [titulo, genero, ano, plataforma]
     # Jogando na matriz
     filmes_series.append(filme_serie)
 
     print(f"\nNovo filme/serie adicionado com sucesso!")
     print(f"{filme_serie[0]} ({filme_serie[2]})")
-    print(f"Gênero: {filme_serie[1]}   Plataforma: {filme_serie[3]}\n")
+    print(f"Gênero: {filme_serie[1]}   Plataforma: {filme_serie[3]}")
 
 if __name__ == "__main__":
     adicionar([['teste', 'qualquer', 2025, 'exemplo']])
